4f011e387fbc2f85eb1571ee260cfa1b4c3701c0
doubletang <doubletang@gmail.com>
2014-05-25 22:46:44 +0800

    Add toppanel

--- main2.py
+++ main2.py
@@ -216,12 +216,15 @@ class Plate(object):
         self.ui = Plate_ui(None, self)
         self.wells = {}
         self.grid = wx.GridSizer(self.n_rows, self.n_columns, 1, 1)
+        n = 0
         for j in range(self.n_rows):
             for i in range(self.n_columns):
                 address = ROW_NAMES[j] + COLUMN_NAMES[i]
                 well = Well(address, self)
                 self.wells[address] = well
                 self.grid.Add(well.ui, 1, wx.EXPAND|wx.ALL, 0)
+                n += 1
+                #self.ui.gauge.SetValue(n)
         print 'Plate inited!'
 
     def getName(self):
@@ -241,10 +244,13 @@ class Plate(object):
     def rangeInPlate(self, myRange):
         return myRange.plate == self
     def initui(self):
-        vbox = wx.BoxSizer(wx.HORIZONTAL)
-        vbox.Add(self.grid, proportion = 1, flag = wx.EXPAND|wx.ALL, border = 10)
-        self.ui.SetSizer(vbox)
-        self.ui.SetSize((self.n_columns * 70, self.n_rows * 48))
+
+        self.ui.vbox.Add(self.grid, proportion = 1, flag=wx.EXPAND|wx.ALL, border = 10)
+
+        #self.ui.SetSizer(vbox)
+
+        self.ui.SetSizer(self.ui.vbox)
+        self.ui.SetSize((self.n_columns * 75, self.n_rows * 60))
         self.ui.SetTitle(self.name)
         self.ui.Centre()
         self.ui.Show()
@@ -415,35 +421,27 @@ class Well_ui(wx.Panel):
         self.Bind(wx.EVT_LEFT_UP, self.OnLeftup)
         self.Bind(wx.EVT_MOTION, self.OnMotion)
         self.Bind(wx.EVT_KEY_DOWN, self.OnKeydown)
-        self.Bind(wx.EVT_CHAR, self.OnChar)
+        self.Bind(wx.EVT_KEY_UP, self.OnKeyup)
         font9 = wx.Font(9, wx.MODERN, wx.SLANT, wx.NORMAL, False, u'Consolas')
         font8 = wx.Font(8, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u'Consolas')
         self.t1 = wx.StaticText(self, label=self.well.address, pos = (1, 0))
         self.t1.SetFont(font9)
         self.t2 = wx.StaticText(self, label=self.well.getSampleNames(), pos = (1, 13))
         self.t2.SetFont(font8)
-        self.t3 = wx.StaticText(self, label=self.well.getDetectorNames(), pos = (1, 26))
-        self.t3.SetFont(font8)
-        self.t3.Wrap(self.GetSize()[0])
         self.t1.Bind(wx.EVT_LEFT_DOWN, self.OnLeftdown)
         self.t2.Bind(wx.EVT_LEFT_DOWN, self.OnLeftdown)
-        self.t3.Bind(wx.EVT_LEFT_DOWN, self.OnLeftdown)
         self.t1.Bind(wx.EVT_LEFT_UP, self.OnLeftup)
         self.t2.Bind(wx.EVT_LEFT_UP, self.OnLeftup)
-        self.t3.Bind(wx.EVT_LEFT_UP, self.OnLeftup)
         self.t1.Bind(wx.EVT_MOTION, self.OnMotion)
         self.t2.Bind(wx.EVT_MOTION, self.OnMotion)
-        self.t3.Bind(wx.EVT_MOTION, self.OnMotion)
-
-    def OnChar(self, e):
-        print 'dfdf'
     
     def OnKeydown(self, e):
-        keymap = {'A' : (0, -1),
-                  'S' : (1, 0),
-                  'D' : (0, 1),
-                  'W' : (-1, 0)}
-        key = chr(e.GetKeyCode())
+        keymap = {65 : (0, -1),
+                  83 : (1, 0),
+                  68 : (0, 1),
+                  87 : (-1, 0)}
+        print e.GetKeyCode()
+        key = e.GetKeyCode()
         a = keymap.get(key)
         if a <> None and self.plate.over_well <> None:
             old_well = self.plate.over_well
@@ -454,6 +452,30 @@ class Well_ui(wx.Panel):
                 new_well.ui.mouseover = True
                 new_well.ui.update()
                 self.plate.over_well = new_well
+                if e.ControlDown():
+                    new_rng = Range(self.plate.start_well, self.plate.over_well)
+                    print new_rng.show()
+                    if self.plate.over_well in self.plate.selected_rng.wells:
+                        for well in self.plate.selected_rng.wells - new_rng.wells:
+                            well.ui.active = False
+                            well.ui.update()
+                    else:
+                        for well in new_rng.wells - self.plate.selected_rng.wells:
+                            well.ui.active = True
+                            well.ui.update()
+                    self.plate.selected_rng = new_rng
+        elif a == None:
+            if key == 308:
+                if self.plate.selected_rng == None:
+                    self.plate.selected_rng = Range(self.plate.over_well, self.plate.over_well)
+                    self.plate.start_well = self.plate.over_well
+                    self.plate.over_well.ui.active = True
+                    self.plate.over_well.ui.update()
+
+    def OnKeyup(self, e):
+        if e.GetKeyCode() == 308:
+            self.OnLeftup(e)
+                  
     
     def OnMotion(self, e):
         #print 'move'
@@ -493,8 +515,15 @@ class Well_ui(wx.Panel):
         else:
             self.color = self.inactive_color
         self.SetBackgroundColour(self.color)
-        self.t2.SetLabel(self.well.getSampleNames())
-        self.t3.SetLabel(self.well.getDetectorNames())
+        if self.Parent.item == 'sample':
+            self.t2.SetLabel(self.well.getSampleNames())
+        elif self.Parent.item == 'detector':
+            self.t2.SetLabel(self.well.getDetectorNames())
+        elif self.Parent.item == 'value':
+            self.t2.SetLabel(self.well.getDetectorValues())
+        self.Parent.Info_Address.SetLabel(self.well.address)
+        self.Parent.Info_Samples.SetLabel(self.well.getSampleNames())
+        self.Parent.Info_Detectors.SetLabel(self.well.getDetectorNames())
         self.Refresh()
 
     def OnLeftdown(self, e):
@@ -507,28 +536,164 @@ class Well_ui(wx.Panel):
         self.update()
     
     def OnLeftup(self, e):
-        self.plate.selected_rng.autoFill(item, entry, direction = 1)
+        self.plate.selected_rng.autoFill(item = self.Parent.item,
+                                         entry = self.Parent.entry,
+                                         index = self.Parent.index[self.Parent.item],
+                                         n_row = self.Parent.Settings_row.GetValue(),
+                                         n_column = self.Parent.Settings_col.GetValue(),
+                                         direction = self.Parent.direction)
+        
         for well in self.plate.selected_rng.wells:
             well.ui.active = False
             well.ui.update()
+        self.plate.selected_rng = None
     
 class Plate_ui(wx.Frame):
     def __init__(self, parent, plate):
         super(Plate_ui, self).__init__(parent)
         self.plate = plate
+        self.item = 'sample'
+        self.index = {'sample' : 0, 'detector' : 0}
+        self.direction = 1
         self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftdown)
         self.Bind(wx.EVT_LEFT_UP, self.OnLeftup)
+        self.entry = sample
+        self.initui()
+        self.bind_event()
+
+    def initui(self):
+        toppanel = wx.Panel(self)
+        #panel.SetBackgroundColour('red')
+        pos = 0
+        hight = 85
+        width = 120
+        gap = 5
+        pos += gap
+        wx.StaticBox(toppanel, label='Raw data', pos=(pos, 0), size=(width, hight))
+        wx.Button(toppanel, label='Load Raw Data', pos=(pos + gap, 30))
+        pos += width
+        
+        width = 85
+        pos += gap
+        wx.StaticBox(toppanel, label='Operation', pos=(pos, 0), size=(width, hight))
+        wx.RadioButton(toppanel, label='Fill', pos=(pos + gap, 20), style=wx.RB_GROUP)
+        wx.RadioButton(toppanel, label='Clear', pos=(pos + gap, 40))
+        pos += width
+        
+        width = 90
+        pos += gap
+        wx.StaticBox(toppanel, label='Items', pos=(pos, 0), size=(width, hight))
+        self.Items_Samples = wx.RadioButton(toppanel, label='Samples', pos=(pos + gap, 20), style=wx.RB_GROUP)
+        self.Items_Detectors = wx.RadioButton(toppanel, label='Detectors', pos=(pos + gap, 40))
+        self.Items_Values = wx.RadioButton(toppanel, label='Values', pos=(pos + gap, 60))
+        pos += width
+        
+        width = 300
+        pos += gap
+        wx.StaticBox(toppanel, label='Settings', pos=(pos, 1), size=(width, hight))
+        wx.StaticText(toppanel, label='Start from:', pos=(pos + gap, 20))
+        wx.StaticText(toppanel, label='Size (x*y):', pos=(pos + gap, 50))
+        pos1 = pos + 80
+        choice_list = []
+        for i in self.entry:
+            choice_list.append(i.name)
+        self.Settings_entry = wx.ComboBox(toppanel, pos=(pos1, 20), size=(80, -1), choices=choice_list, style=wx.CB_READONLY)
+        self.Settings_entry.SetValue(choice_list[0])
+        self.Settings_col = wx.SpinCtrl(toppanel, value='2', pos=(pos1, 50), size=(40, -1))
+        self.Settings_col.SetRange(1, self.plate.n_columns)
+        pos1 += 42
+        wx.StaticText(toppanel, label='x', pos=(pos1, 50))
+        pos1 += 8
+        self.Settings_row = wx.SpinCtrl(toppanel, value='2', pos=(pos1, 50), size=(40, -1))
+        self.Settings_row.SetRange(1, self.plate.n_rows)
+        pos1 += 55
+        wx.StaticText(toppanel, label='Direction:', pos=(pos1, 20))
+        pos1 += 5
+        self.Settings_Direction1 = wx.RadioButton(toppanel, label='Top to botton', pos=(pos1, 40), style=wx.RB_GROUP)
+        self.Settings_Direction2 = wx.RadioButton(toppanel, label='Left to right', pos=(pos1, 60))
+        pos += width
+        
+        width = 200
+        pos += gap
+        wx.StaticBox(toppanel, label='Info', pos=(pos, 1), size=(width, hight))
+        wx.StaticText(toppanel, label='Address:', pos=(pos + gap, 20))
+        wx.StaticText(toppanel, label='Samples:', pos=(pos + gap, 40))
+        wx.StaticText(toppanel, label='Detectors:', pos=(pos + gap, 60))
+        pos1 = pos + 70
+        self.Info_Address = wx.StaticText(toppanel, label='NA', pos=(pos1, 20))
+        self.Info_Samples = wx.StaticText(toppanel, label='NA', pos=(pos1, 40))
+        self.Info_Detectors = wx.StaticText(toppanel, label='NA', pos=(pos1, 60))
+        pos += width
+
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
+        self.vbox.Add(toppanel, 0, flag=wx.EXPAND|wx.LEFT|wx.TOP|wx.RIGHT, border = 0)
+        
+    def bind_event(self):
+        self.Items_Samples.Bind(wx.EVT_RADIOBUTTON, self.OnItemchange)
+        self.Items_Detectors.Bind(wx.EVT_RADIOBUTTON, self.OnItemchange)
+        self.Items_Values.Bind(wx.EVT_RADIOBUTTON, self.OnItemchange)
+        self.Settings_Direction1.Bind(wx.EVT_RADIOBUTTON, self.OnDirectionchange)
+        self.Settings_Direction2.Bind(wx.EVT_RADIOBUTTON, self.OnDirectionchange)
+
+    def OnItemchange(self, e):
+        s = self.Items_Samples.GetValue()
+        d = self.Items_Detectors.GetValue()
+        v = self.Items_Values.GetValue()
+        if s:
+            self.item = 'sample'
+            self.entry = sample
+            for well in self.plate.wells.values():
+                well.ui.t2.SetLabel(well.getSampleNames())
+                well.ui.Refresh()
+            self.Settings_entry.Clear()
+            choice_list = []
+            for i in self.entry:
+                choice_list.append(i.name)
+                self.Settings_entry.Append(i.name)
+            self.Settings_entry.SetValue(choice_list[0])
+        elif d:
+            self.item = 'detector'
+            self.entry = detector
+            for well in self.plate.wells.values():
+                well.ui.t2.SetLabel(well.getDetectorNames())
+                well.ui.Refresh()
+            self.Settings_entry.Clear()
+            choice_list = []
+            for i in self.entry:
+                choice_list.append(i.name)
+                self.Settings_entry.Append(i.name)
+            self.Settings_entry.SetValue(choice_list[0])
+        elif v:
+            self.item = 'value'
+            self.entry = []
+            for well in self.plate.wells.values():
+                well.ui.t2.SetLabel(well.getDetectorValues())
+                well.ui.Refresh()
+            self.Settings_entry.Clear()
+
+    def OnDirectionchange(self, e):
+        d1 = self.Settings_Direction1.GetValue()
+        d2 = self.Settings_Direction2.GetValue()
+        if d1:
+            self.direction = 1
+        elif d2:
+            self.direction = 2
+
 
     def OnLeftdown(self, e):
         self.plate.selected_rng = None
         
     def OnLeftup(self, e):
-        #self.range = []
-        self.plate.selected_rng.autoFill(item, entry, direction = 1)
+        self.plate.selected_rng.autoFill(item = self.item,
+                                         entry = self.entry,
+                                         index = self.index[self.Parent.item],
+                                         n_row = self.Settings_row.GetValue(),
+                                         n_column = self.Settings_col.GetValue(),
+                                         direction = self.direction)
         for well in self.plate.selected_rng.wells:
             well.ui.active = False
             well.ui.update()
-        
+        self.plate.selected_rng = None
 
 def main():
 
@@ -547,8 +712,11 @@ if __name__ == '__main__':
     null = Group('Null').addSampleList(s.subset(11,10))
     neg = Group('Neg').addSampleList(s.subset(21,10))
     shRNA = Group('shRNA').addSampleList(s.subset(31,10))
-    item = 'sample'
-    entry = naive.samples + null.samples + neg.samples + shRNA.samples
+    sample = naive.samples + null.samples + neg.samples + shRNA.samples
+    tbp = Detector('Tbp')
+    slc13a5 = Detector('NaCT')
+    gapdh = Detector('Gapdh')
+    detector = [tbp, slc13a5, gapdh]
     main()
 
 #a = Plate('myplate',16,24)
