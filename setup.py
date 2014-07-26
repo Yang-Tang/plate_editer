ac95a1b406ab2fdcb3638dae7cb8bd6cb4231386
doubletang <doubletang@gmail.com>
2014-07-26 17:07:20 +0800

    Create setup.py
    cmd:
    python setup.py py2exe

---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+ 
+from distutils.core import setup
+import py2exe
+
+setup(
+    windows = [
+    {
+        "script": "main1.py",
+#        "icon_resources": [(1, "app.ico")]
+    }
+    ],
+    options = {
+        "py2exe": {
+            "dll_excludes": ["MSVCP90.dll"]
+        }
+    }
+)
